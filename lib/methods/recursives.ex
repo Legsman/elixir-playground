df950303fe42185bfbae744da24de3340f6a7442
Antoine Macia <antoine@discolabs.com>
2018-02-18 23:09:19 -0500

    Recursives methods: Add range/2 function

--- lib/methods/recursives.ex
+++ lib/methods/recursives.ex
@@ -1,18 +1,41 @@
 defmodule Recursives do
 
   ##
+  # list_len/1
   # Function that calculates the length of a list
+
   def list_len(list) do
     sum_length(0, list)
   end
 
-  ### PRIVATE
+  defp sum_length(sum, []), do: sum
 
-    defp sum_length(sum, []), do: sum
+  defp sum_length(sum, [_head | tail]) do
+    sum + 1
+    |> sum_length(tail)
+  end
 
-    defp sum_length(sum, [_head | tail]) do
-      sum + 1
-      |> sum_length(tail)
-    end
+  ##
+  # range/2
+  # Function that takes 2 integers and returns a list of all numbers in a given range
+
+  def range(from, to) do
+    make_range(from, to, [])
+  end
+
+  # At this point we have the result
+  defp make_range(from, to, result) when from > to do
+    result
+  end
+
+  defp make_range(from, to, result) do
+    # - We first prepend `to` to the top of the accumulated list.
+    # - Then, we just loop the recursion with decremented `to`.
+    # - Since this is the last thing done in the function, it's a tail-recursive
+    #   call.
+    # - Since we continuously prepend decrementing `to`s to the result, we'll
+    #   get the result in the proper order.
+    make_range(from, to - 1, [to | result])
+  end
 
 end
--- test/methods/recursives_test.exs
+++ test/methods/recursives_test.exs
@@ -2,8 +2,13 @@ defmodule RecursivesTest do
   use ExUnit.Case
   doctest Recursives
 
-  test "list_len function calculates length of a list" do
+  test "list_len/1 function calculates length of a list" do
     assert Recursives.list_len([1, :hey, 3, 6, 7]) == 5
     assert Recursives.list_len([]) == 0
+  end
+
+  test "range/2 returns a list of all numbers in a given range" do
+    assert Recursives.range(1, 5) == [1,2,3,4,5]
+    assert Recursives.range(3, 0) == []
   end
 end
