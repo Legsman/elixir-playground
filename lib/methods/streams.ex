c84013e7a27f7be216c115e7281feb96172db54a
Antoine Macia <antoine@discolabs.com>
2018-02-24 17:20:02 -0500

    Streams: Add longest_line/1 function

--- lib/methods/streams.ex
+++ lib/methods/streams.ex
@@ -1,11 +1,13 @@
 defmodule Streams do
 
+  require IEx
+
   ##
   # line_lengths/1
   # Takes a file path and return a list of numbers, with each number representing the length
   # of the corresponding line from the file
   def line_lengths(path) do
-    File.stream!(path)
+    filtered_lines!(path)
     |> Enum.map(&String.length(&1))
   end
 
@@ -13,8 +15,32 @@ defmodule Streams do
   # longest_line_length/1
   # Returns the length of the longest line in a given file
   def longest_line_length(path) do
-    File.stream!(path)
+    filtered_lines!(path)
     |> Stream.map(&String.length(&1))
     |> Enum.max(fn -> nil end)
   end
+
+  ##
+  # longest_line/1
+  # Returns the content of the longest line in a given file
+  def longest_line(path) do
+    filtered_lines!(path)
+    # in this case, the longer_line function call result in longer_line(&1, ""),
+    # with "" being the accumulator we've choosen, which will eventualy immediatly be replaced
+    # by the first longer line with give and will become the new element of comparision while looping
+    |> Enum.reduce("", &longer_line/2)
+  end
+
+    defp filtered_lines!(path) do
+      File.stream!(path)
+      |> Stream.map(&String.replace(&1, "\n", ""))
+    end
+
+    defp longer_line(line1, line2) do
+      if String.length(line1) > String.length(line2) do
+        line1
+      else
+        line2
+      end
+    end
 end
--- test/methods/streams_test.exs
+++ test/methods/streams_test.exs
@@ -5,12 +5,17 @@ defmodule StreamsTest do
 
   test "line_lengths/1 function returns list of lengths of each line of a file" do
     assert Streams.line_lengths(csv_fixture("empty")) == []
-    assert Streams.line_lengths(csv_fixture("lengthy")) == [203, 193, 100, 191, 197]
+    assert Streams.line_lengths(csv_fixture("lengthy")) == [14, 14, 13, 13, 29]
   end
 
   test "longest_line_length/1 function returns the length of longest line in a file" do
     assert Streams.longest_line_length(csv_fixture("empty")) == nil
-    assert Streams.longest_line_length(csv_fixture("lengthy")) == 203
+    assert Streams.longest_line_length(csv_fixture("lengthy")) == 29
+  end
+
+  test "longest_line/1 function returns the content of longest line in a file" do
+    assert Streams.longest_line(csv_fixture("empty")) == ""
+    assert Streams.longest_line(csv_fixture("lengthy")) == "Classic Hardcore Sheet Bundle"
   end
 
 end
