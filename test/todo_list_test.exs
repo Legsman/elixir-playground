c514ebc65cfdda0f3bb998828629d2acef8aab67
Antoine Macia <antoine@discolabs.com>
2018-03-10 14:57:04 -0500

    TodoList - Refactor list to use auto id as keys

--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -6,7 +6,7 @@ defmodule TodoListTest do
   test "can add and read entry to todo_list" do
     todo_list = TodoList.new
     |> TodoList.add_entry(%{ date: {2017, 09, 10}, title: "New Entry"})
-    assert todo_list == %{{2017, 9, 10} => [%{date: {2017, 9, 10}, title: "New Entry"}]}
+    assert todo_list == %TodoList{auto_id: 2, entries: %{1 => %{date: {2017, 9, 10}, id: 1, title: "New Entry"}}}
   end
 
   test "can read entries to todo_list" do
@@ -17,6 +17,6 @@ defmodule TodoListTest do
     ]
     todo_list = Enum.reduce(entries, TodoList.new, &TodoList.add_entry(&2, &1))
 
-    assert TodoList.entries(todo_list, {2017, 9, 11}) == [%{date: {2017, 9, 11}, title: "New Entry 2"}]
+    assert TodoList.entries(todo_list, {2017, 9, 11}) == [%{date: {2017, 9, 11}, id: 2, title: "New Entry 2"}]
   end
 end
