c57dafeb92a6d24c20c5600dbd36f6059bc7acc7
Antoine Macia <antoine@discolabs.com>
2018-04-23 19:25:12 -0400

    Identicon - Add initial structure and hash gen

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -1,18 +1,24 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Turn a string into an Identicon
   """
+
+  def main(input) do
+    input
+    |> hash_input
+  end
 
   @doc """
-  Hello world.
+  Hash given string using md5 encryption, and insert the
+  15 binaries produced into a list
 
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
+    ## Examples
+      iex> Identicon.hash_input('salut')
+      [62, 215, 220, 234, 242, 102, 202, 254, 240, 50, 185, 213, 219, 34, 71, 23]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
+
 end
---
+++ identicon/lib/image.ex
@@ -0,0 +1,6 @@
+defmodule Identicon.Image do
+  defstruct hex: nil
+  @moduledoc """
+  Data structure the Identicon
+  """
+end
