43da0732d97ea8dfd13aec7f10849581954ef6be
Antoine Macia <antoine@discolabs.com>
2018-05-06 19:50:30 -0400

    Identicon - Build pixel map from grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -9,6 +9,8 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+    |> build_pixel_map
   end
 
   @doc """
@@ -74,6 +76,26 @@ defmodule Identicon do
     end)
 
     %Identicon.Image{ image | grid: grid }
+  end
+
+  @doc """
+  Each square from the grid should be 50x50 tall and wide,
+  this function calculates the x and y coordinates for each grid squares
+  based on its index.
+
+  Returns the Imager struct with the calculated pixel_map.
+  """
+  def build_pixel_map(%Identicon.Image{ grid: grid } = image) do
+    pixel_map = Enum.map(grid, fn({_val, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
+
+    %Identicon.Image{ image | pixel_map: pixel_map }
   end
 
 end
