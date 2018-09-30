6bd37d4ff6236501cb721edeaa508237fee273ca
Antoine Macia <antoine@discolabs.com>
2018-09-30 17:21:36 +1000

    Revert Ecto initial setup

--- discuss/lib/discuss.ex
+++ discuss/lib/discuss.ex
@@ -9,7 +9,7 @@ defmodule Discuss do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      # supervisor(Discuss.Repo, []),
+      supervisor(Discuss.Repo, []),
       # Start the endpoint when the application starts
       supervisor(Discuss.Endpoint, [])
       # Start your own worker by calling: Discuss.Worker.start_link(arg1, arg2, arg3)
